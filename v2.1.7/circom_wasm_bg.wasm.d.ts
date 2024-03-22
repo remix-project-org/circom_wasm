fe4cb52607de6c4a917932d200ff46c2752ee635
ioedeveloper <daviddisu8@gmail.com>
2024-03-22 12:44:40 +0100

    Add new wasm compiler build v2.1.7

---
+++ v2.1.7/circom_wasm_bg.wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_compilationresult_free(a: number): void;
+export function compilationresult_program(a: number): number;
+export function compilationresult_input_signals(a: number, b: number, c: number): number;
+export function compilationresult_report(a: number): number;
+export function compilationresult_log(a: number): number;
+export function __wbg_r1csresult_free(a: number): void;
+export function r1csresult_program(a: number): number;
+export function r1csresult_report(a: number): number;
+export function r1csresult_log(a: number): number;
+export function __wbg_parseresult_free(a: number): void;
+export function parseresult_report(a: number): number;
+export function parseresult_get_report_name(a: number, b: number): number;
+export function compile(a: number, b: number, c: number, d: number): number;
+export function parse(a: number, b: number, c: number): number;
+export function generate_r1cs(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_exn_store(a: number): void;
